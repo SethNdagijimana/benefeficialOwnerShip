6f71d5972ca8cd56fef14dcc5d9d244b26067e7f
sethndagijimana <sethreas@gmail.com>
2024-05-17 12:59:56 +0200

    added footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+
+        <>
+            <div className="mt-16 p-8">
+           <p className="text-[#014D99] text-center">
+           Company Code/TIN Nº: 103752228 | RSE Ticker: BOKGI | Capital: FRW 5,000,000,000 | Swift: BKGIRWRW P.O. Box 724 Kigali | KN 3 Ave, Kigali | Tel: (+250) 788 177 400 | Cell: (+250) 788 384 099 
+           </p>
+           <p className="text-[#014D99] text-center">
+           <a href="mailto:bkinsurance@bk.rw">Email: bkinsurance@bk.rw</a>
+           </p>
+
+
+          <p className="text-[#014D99] text-center">
+            <a href="https://www.bkinsurance.rw" target="_blank" rel="noopener noreferrer">Website: www.bkinsurance.rw</a>
+        </p>
+            </div>
+        </>
+    )   
+}
+
+export default Footer
